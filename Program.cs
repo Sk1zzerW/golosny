bfd24c9e6e19637f63e3decc319ba39e3a0c7ba4
olegk <kbryzhenyukpc@gmail.com>
2024-11-15 23:17:38 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ваше предложение");
+        string input = Console.ReadLine();
+
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y', 'а', 'е', 'и', 'о', 'у', 'я', 'ю', 'ё', 'э', 'ы' };
+
+        input = input.ToLower();
+
+        int totalVowels = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            totalVowels += Array.IndexOf(vowels, input[i]) >= 0 ? 1 : 0;
+        }
+
+        Console.WriteLine("гласных букв -  " + totalVowels);
+
+    }
+}
